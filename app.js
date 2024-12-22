0192cd7a7a88ceb45b9a183b2b1a26fdeceb585e
sanket-67 <sanketmule67@gmail.com>
2024-12-22 15:16:40 +0530

    Updated project with fixed build scripts and dependencies

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ import {mongoCon} from './db/mongocon.js'
 import cors from 'cors'
 import { projectroute } from './routes/Projectroute.js';
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+const allowedOrigins = [
+    'https://projecthub1.vercel.app', 
+    'http://localhost:3000'          
+  ];
+  
+  app.use(cors({
+    origin: allowedOrigins, 
+    credentials: true       
+  }));
 dotenv.config();
 mongoCon()
 app.use(express.json())
