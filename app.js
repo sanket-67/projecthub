fda40fbe6aed124013207f2b7582cd9e72f5498c
Sanket ramrao mule <149441904+sanket-67@users.noreply.github.com>
2024-12-25 14:34:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,14 +16,9 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['set-cookie']
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+
 
 dotenv.config();
 mongoCon();
