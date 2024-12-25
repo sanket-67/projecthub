72766db29110837bcab72e3fd219aae7e3965ea1
Sanket ramrao mule <149441904+sanket-67@users.noreply.github.com>
2024-12-25 18:34:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const app = express();
 app.use(cors({
     origin: ['https://projecthub1.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+
 }));
 
 
